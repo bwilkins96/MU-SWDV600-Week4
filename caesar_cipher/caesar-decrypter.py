d7d1c31a949d95f9e78be5486fc93fc9500a15a1
bwilkins96 <ben.jr.speed@gmail.com>
2023-02-08 15:55:17 -0500

    caesar cipher

--- caesar_cipher/caesar-decrypter.py
+++ caesar_cipher/caesar-decrypter.py
@@ -2,6 +2,27 @@
 # with an inputted shift key and prints it to the shell
 
 def main():
-    pass
+    input_file_name = input('Enter the encrypted message file name: ')
+    key = int(input('Enter the shift key: ')) 
+    # use 3 for example.txt
+
+    alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[len(alphabet) - key:] + alphabet[:len(alphabet) - key]
+
+    input_file = open(input_file_name, 'r')
+    encrypted_message = input_file.read()
+
+    message = ''
+    for char in encrypted_message.strip():
+        letter_index = shifted_alphabet.find(char)
+        decrypted_char = alphabet[letter_index]
+
+        if letter_index > -1:
+            message += decrypted_char
+        else:
+            message += char
+
+    input_file.close()
+    print(f'\nThe message is: {message}')    
 
 main()
--- caesar_cipher/caesar-encrypter.py
+++ caesar_cipher/caesar-encrypter.py
@@ -2,6 +2,27 @@
 # with an inputted shift key and stores encrypted message in a file
 
 def main():
-    pass
+    message = input('Enter the message to encrypt: ')
+    key = int(input('Enter the shift key: '))
+    output_file_name = input('Enter the output file name: ')
+
+    alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[len(alphabet) - key:] + alphabet[:len(alphabet) - key]
+
+    encrypted_message = ''
+    for char in message:
+        letter_idx = alphabet.find(char)
+        encrypted_char = shifted_alphabet[letter_idx]
+
+        if letter_idx > -1:
+            encrypted_message += encrypted_char
+        else:
+            encrypted_message += char
+
+    output_file = open(output_file_name, 'w')
+    print(encrypted_message, file=output_file)
+    output_file.close()
+        
+    print(f'Done writing encrypted message to {output_file_name}')
 
 main()
